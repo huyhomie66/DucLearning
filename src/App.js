8d10bdd01229f4ba59a2a0ce4ce2b80f812382a1
liam <liam@liams-Mac-mini.local>
2021-06-27 15:48:37 +0700

    duc ha

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useContext, useEffect, useState } from "react";
+import { search, providers } from "./service/search";
 
 function App() {
+  const [providerValue, setProviderValue] = useState("");
+  const [districtValue, setDistrictValue] = useState();
+  const [totalValue, setTotalValue] = useState("");
+  const [suggestion, setSuggestion] = useState(providers);
+
+  const providerProps = {
+    value: providerValue,
+    onChange: (e) => setProviderValue(e.target.value),
+  };
+
+  const districtProps = {
+    value: districtValue,
+    onChange: (e) => setDistrictValue(e.target.value),
+  };
+
+  useEffect(() => {
+    const suggestion = search(providerValue);
+    console.log(suggestion);
+    setSuggestion(suggestion);
+  }, [providerValue]);
+
+  // const suggestion = searchResult;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <input placeholder="Tìm kiếm tỉnh" {...providerProps} />
+
+      <div>
+        {suggestion.map((e) => (
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            <div>{e.name}</div>
+          </div>
+        ))}
+      </div>
+      {/* <input placeholder="Tìm kiếm quận huyện " {...districtProps} />
+      <div>
+        {suggestion.map((e) => (
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            <div>{e.name}</div>
+          </div>
+        ))}
+      </div> */}
     </div>
   );
 }
